aec7080f92cae8d8eceae33cae82218023cc1255
Rafia Zafar <rafiazafar75@gmail.com>
2021-08-24 00:06:54 +0500

    addin the about us section

--- src/components/About-me/About.js
+++ src/components/About-me/About.js
@@ -5,7 +5,30 @@ import './About.css'
 function About() {
     return (
         <div className="about-me">
-                <h1 className="about-me_header p-0">About me</h1>
+
+            <div className="about-me_header ">
+            <h1 className="header--header">About me</h1>
+            </div>
+
+            <p className="about-me__paragraph">
+            <span className="paragraph--bold">I'm Rafia .Z, a web developer enthusiast.</span> text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type 
+            specimen book. It has survived not only five centuries,but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem
+            </p>
+
+            <div className="about-me__sections">
+             <div className="section--personal-details">
+
+             </div>
+
+             <div className="section--my-interests">
+
+             </div>
+
+            </div>
+                
         </div>
     )
 }
