94019522d7d27c0612e87c14384f342192504af1
Rafia Zafar <rafiazafar75@gmail.com>
2021-08-27 20:04:48 +0500

    correcting things

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Navbar from './components/header/Navbar';
 import './App.css';
 import Header from './components/header/Header';
 import About from './components/About-me/About';
-import Popup from './components/header/Popup';
+
 
 
 
@@ -12,7 +12,7 @@ function App(props) {
       <Navbar/>
       <Header/>
       <About/>
-      <Popup/>
+ 
     </div>
   );
 }
