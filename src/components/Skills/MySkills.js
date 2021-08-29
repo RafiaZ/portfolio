8ecafb4b360d68183ddf0bba3bf441f8417bf9b3
Rafia Zafar <rafiazafar75@gmail.com>
2021-08-29 23:45:41 +0500

    progress bars

--- src/components/Skills/MySkills.js
+++ src/components/Skills/MySkills.js
@@ -8,7 +8,35 @@ function MySkills() {
     return (
         <div className="skills">
 
-            <div className="skills__content">
+            <div className="containers skills-content">
+
+            <div className="skills_header ">
+            <h1 className="skills_header-underline">Skills</h1>
+            </div> 
+
+            <div className="skills_bars_section">
+                <div className="section_left">
+                <ProgressBar now={60} /> 
+               <h4>Html</h4>
+               <ProgressBar now={60} /> 
+               <h4>Html</h4>
+            
+                </div>
+                <div className="section_right">
+                <ProgressBar now={60} /> 
+                <h4>css</h4>
+                </div>
+            </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default MySkills
+
+
+{/* <div className="skills__content">
             
             <div className="skills__header">
              <h1 className="skills__header-heading"> Skills </h1>
@@ -27,9 +55,4 @@ function MySkills() {
            
 
 
-            </div> 
-        </div>
-    )
-}
-
-export default MySkills
+            </div>  */}
