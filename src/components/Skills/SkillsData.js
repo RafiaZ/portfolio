6139299ff59ad738c3118903935b72e5834ab2ef
Rafia Zafar <rafiazafar75@gmail.com>
2021-08-30 23:39:24 +0500

    affig

---
+++ src/components/Skills/SkillsData.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        "percentage" : "30",
+        "label" : "html",
+        "id" : "1"
+    }, 
+    {
+        "percentage" : "20",
+        "label" : "Javascript",
+        "id" : "2"
+    }, 
+    {
+        "percentage" : "80",
+        "label" : "bootstrap",
+        "id" : "3"
+    }, 
+    {
+        "percentage" : "70",
+        "label" : "SCSS",
+        "id" : "4"
+    }, 
+]
