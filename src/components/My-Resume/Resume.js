723d8f4a775bacebeb422e93c01be3d9d23593ca
Rafia Zafar <rafiazafar75@gmail.com>
2021-09-04 00:07:19 +0500

    adding new component

---
+++ src/components/My-Resume/Resume.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './Resume.css'
+
+function Resume() {
+    return (
+        <div>
+            <div className="resume-container">
+                <div className="resume-left-part">
+                    <div className="resume-left-part"></div>
+                </div>
+                <div className="resume-left-part"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Resume
