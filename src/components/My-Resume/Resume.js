61824081bfb703f418fccd9ab5008e641d3f4f85
Rafia Zafar <rafiazafar75@gmail.com>
2021-09-08 00:25:20 +0500

    adding

--- src/components/My-Resume/Resume.js
+++ src/components/My-Resume/Resume.js
@@ -6,12 +6,28 @@ function Resume() {
     return (
         <div className="resume-container">
             <div className="resume-container-inner">
+                
               <div className="resume-header">
                   <h1 className="resume-header-underline">My Experiene</h1>
               </div>
 
-              <div className="resume-cards">
-                  
+              <div className="experience-cards">
+                  <div className="card">
+                      <div className="card-heading">
+                          <h3> Experiene no 1</h3>
+                      </div>
+                      <p>Here I worked</p>
+                      <p>2002-2004</p>
+                  </div>
+
+
+                  <div className="card">
+                  <div className="card-heading">
+                          <h3> Experiene no 1</h3>
+                      </div>
+                      <p>Here I worked</p>
+                      <p>2002-2004</p>
+                  </div>
               </div>
 
             </div>
