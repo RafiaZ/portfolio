17dd898d017e57156a1624e38ca0d7c8b4cc0c24
Rafia Zafar <rafiazafar75@gmail.com>
2021-09-12 21:52:27 +0500

    adding

--- src/components/Recent-work/recents.js
+++ src/components/Recent-work/recents.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Recents.css'
 
 function Recents() {
     return (
-        <div className="recents-container">
-            
+        <div className="recents__container">
+            <div className="recents__header">
+                <h1 className="recents__header-heading">Recent Portfolio</h1>
+            </div>
+            <div className="recents__list">
+
+              <li> <a href="#" className="recents__list-link">All catagories</a></li>
+              <li> <a href="#" className="recents__list-link">My Blog</a></li>
+              <li> <a href="#" className="recents__list-link">Photography</a></li>
+              <li> <a href="#" className="recents__list-link">Painting</a></li>
+                
+            </div>
+            <div className="recents__grid">
+                <div></div>
+            </div>
         </div>
     )
 }
