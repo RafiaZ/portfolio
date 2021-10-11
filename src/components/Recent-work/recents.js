68791e6b86bc9b0fae4e514c590eeeb89449a555
Rafia Zafar <rafiazafar75@gmail.com>
2021-10-11 18:59:00 +0500

    adding grid

--- src/components/Recent-work/recents.js
+++ src/components/Recent-work/recents.js
@@ -1,70 +1,95 @@
-import React from 'react'
+import React from "react";
 
-import ImageRecent from './recents-images/r-img-1.jpg'
-import './Recents.css'
+import ImageRecent from "./recents-images/r-img-1.jpg";
+import "./Recents.css";
 
 function Recents() {
-    return (
-        <div className="recents__container">
-            <div className="recents__header">
-                <h1 className="recents__header-heading">Recent Portfolio</h1>
-            </div>
-            <div className="recents__list">
+  return (
+    <div className="recents__container">
+      
+      <div className="recents__header">
+        <h1 className="recents__header-heading">Recent Portfolio</h1>
+      </div>
+      <div className="recents__list">
+        <li className="recents__list-item">
+          {" "}
+          <a href="#" className="recents__list-link">
+            All catagories
+          </a>
+        </li>
+        <li className="recents__list-item">
+          {" "}
+          <a href="#" className="recents__list-link">
+            My Blog
+          </a>
+        </li>
+        <li className="recents__list-item">
+          {" "}
+          <a href="#" className="recents__list-link">
+            Photography
+          </a>
+        </li>
+        <li className="recents__list-item">
+          {" "}
+          <a href="#" className="recents__list-link">
+            Painting
+          </a>
+        </li>
+      </div>
 
-              <li className="recents__list-item"> <a href="#" className="recents__list-link">All catagories</a></li>
-              <li className="recents__list-item"> <a href="#" className="recents__list-link">My Blog</a></li >
-              <li className="recents__list-item"> <a href="#" className="recents__list-link">Photography</a></li >
-              <li className="recents__list-item"> <a href="#" className="recents__list-link">Painting</a></li>
-                
-            </div>
-            <div className="recents__grid">
-                
-                <div  className="recents__images">
-                    <img src={ImageRecent} alt="recents image 1"/>
-                </div>
-
-                <div  className="recents__images">
-                    <img  src={ImageRecent} alt="recents image 1"/>
-                </div>
-
-                <div  className="recents__images">
-                    <img  src={ImageRecent} alt="recents image 1"/>
-                </div>
-
-                <div  className="recents__images">
-                    <img  src={ImageRecent} alt="recents image 1"/>
-                </div>
-
-                <div  className="recents__images">
-                    <img  src={ImageRecent} alt="recents image 1"/>
-                </div>
-
-                <div  className="recents__images">
-                    <img  src={ImageRecent} alt="recents image 1"/>
-                </div>
-
-                <div  className="recents__images">
-                    <img  src={ImageRecent} alt="recents image 1"/>
-                </div>
-
-                <div  className="recents__images">
-                    <img  src={ImageRecent} alt="recents image 1"/>
-                </div>
-                <div  className="recents__images">
-                    <img  src={ImageRecent} alt="recents image 1"/>
-                </div>
-                <div  className="recents__images">
-                    <img  src={ImageRecent} alt="recents image 1"/>
-                </div>
-                <div  className="recents__images">
-                    <img  src={ImageRecent} alt="recents image 1"/>
-                </div>
-
-               
-                
-            </div>
+      <div className="recents__grid">
+        <div>
+          <img
+            className="recents__images"
+            src={ImageRecent}
+            alt="recents image 1"
+          />
         </div>
-    )
+
+        <div>
+          <img
+            className="recents__images"
+            src={ImageRecent}
+            alt="recents image 1"
+          />
+        </div>
+
+        <div>
+          <img
+            className="recents__images"
+            src={ImageRecent}
+            alt="recents image 1"
+          />
+        </div>
+
+        <div>
+          <img
+            className="recents__images"
+            src={ImageRecent}
+            alt="recents image 1"
+          />
+        </div>
+
+        <div>
+          <img
+            className="recents__images"
+            src={ImageRecent}
+            alt="recents image 1"
+          />
+        </div>
+
+        <div>
+          <img
+            className="recents__images"
+            src={ImageRecent}
+            alt="recents image 1"
+          />
+        </div>
+
+
+      </div>
+    </div>
+  );
 }
 
-export default Recents
+export default Recents;
