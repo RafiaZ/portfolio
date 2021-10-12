3b9d65302125a8c5b9b3ab80ce76459477fa491a
Rafia Zafar <rafiazafar75@gmail.com>
2021-10-12 19:10:47 +0500

    added pictures

--- src/components/Recent-work/RecentArray.js
+++ src/components/Recent-work/RecentArray.js
@@ -1,4 +1,14 @@
 import ImageRecent from "./recents-images/r-img-1.jpg";
+
+import blog_1 from "./recents-images/blog_1.jpg";
+import blog_2 from "./recents-images/blog_2.jpg";
+import blog_3 from "./recents-images/blog_3.jpg";
+
+import photog_1 from "./recents-images/photog_1.jpg";
+import photog_2 from "./recents-images/photog_2.jpg";
+import photog_3 from "./recents-images/photog_3.jpg";
+
+
 export const AllCatagories = [
     {
       src: ImageRecent,
@@ -15,6 +25,44 @@ export const AllCatagories = [
     {
       src:ImageRecent,
       alt: "image 1",
-    },
+    }
 
+  ]
+
+  export const MyBlog = [
+    {
+      src: blog_1,
+      alt: "image 1",
+    },
+    {
+      src: blog_2,
+      alt: "image 1",
+    },
+    {
+      src:blog_3,
+      alt: "image 1",
+    },
+    {
+      src:ImageRecent,
+      alt: "image 1",
+    }
+  ]
+
+  export const Photography = [
+    {
+      src: photog_1,
+      alt: "image 1",
+    },
+    {
+      src: photog_2,
+      alt: "image 1",
+    },
+    {
+      src: photog_3,
+      alt: "image 1",
+    },
+    {
+      src:ImageRecent,
+      alt: "image 1",
+    }
   ]
